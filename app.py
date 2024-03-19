6836db9771e5d924a62203e0e4c8efc0551f44b8
Meed <mohamedouazane@yahoo.com>
2024-03-19 00:21:39 +0000

    change ip and add port

--- app.py
+++ app.py
@@ -182,4 +182,6 @@ def add_bmi():
 
 if __name__ == "__main__":
     """ Main Function """
-    app.run(host='10.204.225.169', port=5000)
+    import os
+    port = int(os.environ.get("PORT", 5000))
+    app.run(host='0.0.0.0', port=port, debug=False)
